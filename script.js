35664370695e8b401ca17607160bab3bf967362e
MohitGeryani <90450290+MohitGeryani@users.noreply.github.com>
2021-09-10 15:50:36 +0530

    A Simple Cloud Storage Site Design

---
+++ script.js
@@ -0,0 +1,28 @@
+// Styling Through Javascript 
+
+var button=document.getElementById('button');
+
+button.addEventListener('click', function(){
+   button.style.color='white';
+   button.style.backgroundColor='green';
+   button.style.border=' px solid green';
+   button.style.borderRadius='';
+   button.style.fontWeight='bold';
+   button.style.boxShadow='0px 0px 0px 4px rgba(0,118,71,0.5)';
+   var i=0;
+   var tt=setInterval(function(){     
+   i=i+1;
+   var counter=1-i;
+   button.innerHTML='Download'
+     if(counter===0){
+       clearInterval(tt);
+       window.location = "/Data/movies/Bollywood/2019/Baazaar%20%282018%29/Baazaar%20%282018%29%201080p.mkv";
+       window.open('https://rsuflix.blogspot.com');
+       return true;
+     }
+     
+   },1200);
+   
+});
+
+// Wait For 1.2 Seconds Before Redirecting
